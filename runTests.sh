87ed6cacb51d7abfa8884a56fe057d3cdadb2718
sporollan <santiago.porollan@gmail.com>
2023-10-13 11:32:03 +0000

    add runtests

---
+++ runTests.sh
@@ -0,0 +1,5 @@
+javac tests/*.java \
+	tests/lineales/dinamicas/TestLista.java lineales/dinamicas/Lista.java lineales/dinamicas/Nodo.java \
+	tests/conjuntistas/*.java \
+	conjuntistas/*.java
+java tests/Main
