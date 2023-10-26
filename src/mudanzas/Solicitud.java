4a7c637198d588c040a2d740f49876b6d5c5f3f1
sporollan <santiago.porollan@gmail.com>
2023-10-26 11:05:36 -0300

    add solicitud

---
+++ src/mudanzas/Solicitud.java
@@ -0,0 +1,32 @@
+package src.mudanzas;
+
+public class Solicitud {
+    Comparable origen;
+    Comparable destino;
+    String nombre;
+
+    public Solicitud(Comparable origen, Comparable destino, String nombre)
+    {   
+        this.origen = origen;
+        this.destino = destino;
+        this.nombre = nombre;
+
+    }
+
+    public int hashCode()
+    {
+        String c = "";
+        int h;
+        c += origen;
+        c += destino;
+        h = c.hashCode();
+        if(h < 0)
+            h *= -1;
+        return h;
+    }
+
+    public String toString()
+    {
+        return this.nombre;
+    }
+}
