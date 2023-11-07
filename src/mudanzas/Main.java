9e53e57eca576c33f4485467f1c6d8d40a199634
sporollan <santiago.porollan@gmail.com>
2023-11-07 16:31:14 -0300

    refactorizacion

--- src/mudanzas/Main.java
+++ src/mudanzas/Main.java
@@ -1,12 +1,10 @@
 package src.mudanzas;
 
-import lib.conjuntistas.ArbolAVL;
 
 public class Main {
     public static void main(String[] args)
     {
         MudanzasCompartidas mc = new MudanzasCompartidas();
-        mc.cargarArchivo("files/inicial.txt");
         mc.run();
     }
 }
