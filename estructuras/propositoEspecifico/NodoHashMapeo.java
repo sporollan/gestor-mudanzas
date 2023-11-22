040770744913ad8bd178416cb659ba0b875c90d0
sporollan <santiago.porollan@gmail.com>
2023-11-22 13:48:19 -0300

    add nodohashmapeo

--- estructuras/propositoEspecifico/NodoHashMapeo.java
+++ estructuras/propositoEspecifico/NodoHashMapeo.java
@@ -1,5 +1,33 @@
 package estructuras.propositoEspecifico;
 
+// clase utilizada por MapeoAUno
 public class NodoHashMapeo {
+    private Object tipoDominio;
+    private Object rango;
+    private NodoHashMapeo enlace;
 
+
+    public NodoHashMapeo(Object tipoDominio, Object rango, NodoHashMapeo enlace) {
+        this.tipoDominio = tipoDominio;
+        this.rango = rango;
+        this.enlace = enlace;
+    }
+    public Object getTipoDominio() {
+        return tipoDominio;
+    }
+    public void setTipoDominio(Object tipoDominio) {
+        this.tipoDominio = tipoDominio;
+    }
+    public Object getRango() {
+        return rango;
+    }
+    public void setRango(Object rango) {
+        this.rango = rango;
+    }
+    public NodoHashMapeo getEnlace() {
+        return enlace;
+    }
+    public void setEnlace(NodoHashMapeo enlace) {
+        this.enlace = enlace;
+    }
 }
